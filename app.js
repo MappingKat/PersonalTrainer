f4196eea5efc7f8629073d60fe85376e98bfe65b
bjurgess1 <jurgessb@msu.edu>
2015-08-22 21:44:25 -0400

    Adding compass;

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./controllers/index');
+var users = require('./controllers/users');
 
 var app = express();
 
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('node-compass')({mode: 'expanded'}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
